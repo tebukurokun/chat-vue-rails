981e09e5949ba18439e4919a2e2fe24c3708d075
tebukuro <58774766+tebukurokun@users.noreply.github.com>
2023-06-26 01:55:11 +0900

    add client

---
+++ chat-client/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view />
+    <ApiTest />
+  </div>
+</template>
+
+<script>
+import ApiTest from './components/ApiTest.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ApiTest
+  }
+}
+</script>
